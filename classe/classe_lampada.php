ce6645a084265940bc056e356a7afeeb528f828c
Marynamore <marinaoliverbri674@gmail.com>
2023-04-21 21:10:33 -0300

    Classe com o metodo Construtor e Destrutores

--- classe/classe_lampada.php
+++ classe/classe_lampada.php
@@ -1,14 +1,18 @@
 <?php
-//Lampada.php
+//Atributo Privado
 class Lampada{
     public $cor;
     public $ligada;
+
+    //Método construtor
     public function __construct($cor){
         $this->cor=$cor;
         $this->ligada=false;
         echo "<div style='color:".$this->cor."'>";
         echo "Uma lampada foi criada !</div><br>";
     }
+
+    //Metodo => função
     function Ligar(){
         if($this->ligada){
             echo "A lampada ja esta ligada";
@@ -18,6 +22,26 @@ class Lampada{
             $this->ligada= true;
         }
     }
+
+    // Metodos acessores de cada atributo
+
+    function getCor(){
+        return $this->cor;
+    }//pegamos os atributos privados e mostramos
+
+    function getLigada(){
+        return $this->ligada;
+    }
+
+    function setCor($cor){
+        $this->cor = $cor;
+    }//setamos os atributos privados
+
+    function setLigar($ligada){
+        $this->ligada = $ligada;
+    }
+
+    //Método desconstrutor
     public function __destruct()
     {
         echo "<br>Lampada ".$this->cor." destruida !<br> ";
